d0775606c83b36b5ba1c7303c1b9f84e4bc90e01
ragulraj <rraj9350@gmail.com>
2025-02-06 18:56:55 +0530

    project commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $('.res-menu').click(function () {
+
+        $('.res-menu-items-wrapper').toggle();
+
+    })
+
+    $('.res-menu-items-wrapper li a').click(function () {
+
+        $('.res-menu-items-wrapper').toggle();
+        
+    })
+
+});
